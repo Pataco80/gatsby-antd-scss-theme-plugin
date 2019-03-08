bbdf2fff8c2ff04dd5fd5ebda5230fbdeb1b4207
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-03-08 16:03:34 +0100

    Ad files from the theme

---
+++ src/theme/antd.js
@@ -0,0 +1,9 @@
+
+// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+module.exports = {
+  "@primary-color": "#745844",
+  "@link-color": "@primary-color",
+  "@font-family": "Courgette",
+  "@layout-body-background": "rgba(116, 88, 68, 0.05)",
+  "@skeleton-color": "@white"
+};
